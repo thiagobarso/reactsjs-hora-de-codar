8c227c7fb53f1c6d04e30b7df6fc4b7fa0e56510
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-29 16:00:29 -0300

    Curso React: Resgatando projetos do banco de dados - #27

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import Message from "../layouts/Message"
 import Container from '../layouts/Container'
 import LinkButton from '../layouts/LinkButton'
 import styles from './Projects.module.css'
+import ProjectCard from "./ProjectCard"
 
 function Projects() {
 
+    const [projects, setProjects] = useState([])
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setProjects(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
 
     return (
         <div className={styles.project_container}>
@@ -21,7 +39,17 @@ function Projects() {
 
             {message && <Message type="success" msg={message} />}
             <Container customClass="start">
-                <p>Projetos</p>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    ))
+                }
             </Container>
         </div>
     )
